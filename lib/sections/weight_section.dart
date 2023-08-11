832335b0fecb3c003ab4e2cb4f45147e97d5514e
Joel <bacierto.joel@gmail.com>
2023-08-11 15:06:58 +0800

    Weight section updated for add function

--- lib/sections/weight_section.dart
+++ lib/sections/weight_section.dart
@@ -8,8 +8,27 @@ class WeightSection extends StatefulWidget {
 }
 
 class _WeightSectionState extends State<WeightSection> {
-  final String _selectedWeightUnits = 'Pounds (lb)';
-  void _weightUnits() {}
+  String _selectedUnit = 'Kilograms (KG)';
+  int _countWeight = 0;
+  final List<String> _weightUnits = [
+    'Kilograms (KG)',
+    'Pounds (lb)',
+  ];
+  // this method is to increase the number of weight
+  void _incCountWeight() {
+    setState(() {
+      _countWeight = _countWeight + 1;
+    });
+  }
+
+  // this method is to decrease the number of weight
+  void _decCountWeight() {
+    if (_countWeight > 0) {
+      setState(() {
+        _countWeight = _countWeight - 1;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,38 +44,65 @@ class _WeightSectionState extends State<WeightSection> {
             color: Colors.white,
             borderRadius: BorderRadius.circular(5),
           ),
-          child: const Row(
+          child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Icon(Icons.do_disturb_on),
-              Text(
-                '0',
-                style: TextStyle(fontWeight: FontWeight.bold),
+              // icon button to decrease number of weight
+              IconButton(
+                onPressed: _decCountWeight,
+                icon: const Icon(Icons.do_disturb_on),
               ),
-              Icon(Icons.add_circle_outlined),
+              // show the number of weight
+              Text(
+                _countWeight.toString(),
+                style: const TextStyle(fontWeight: FontWeight.bold),
+              ),
+              // icon button to increase number of weight
+              IconButton(
+                onPressed: _incCountWeight,
+                icon: const Icon(Icons.add_circle_outlined),
+              ),
             ],
           ),
         ),
         // units of weight section
-        GestureDetector(
-          onTap: _weightUnits,
-          child: Container(
-            padding: const EdgeInsets.symmetric(horizontal: 5),
-            width: 150,
-            height: 35,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(5),
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  _selectedWeightUnits,
-                  style: const TextStyle(fontWeight: FontWeight.bold),
-                ),
-                const Icon(Icons.arrow_drop_down_circle)
-              ],
+        Container(
+          padding: const EdgeInsets.symmetric(horizontal: 5),
+          width: 165,
+          height: 35,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(5),
+          ),
+          child: DropdownButtonHideUnderline(
+            child: DropdownButton(
+              value: _selectedUnit,
+              icon: const Icon(
+                Icons.arrow_drop_down_circle_rounded,
+                color: Colors.black,
+              ),
+              items: _weightUnits
+                  .map(
+                    (units) => DropdownMenuItem(
+                      value: units,
+                      alignment: AlignmentDirectional.center,
+                      child: Text(
+                        units,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  )
+                  .toList(),
+              onChanged: (value) {
+                if (value == null) {
+                  return;
+                }
+                setState(() {
+                  _selectedUnit = value;
+                });
+              },
             ),
           ),
         ),
